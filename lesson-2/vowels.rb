4febeb03ae9b46acae6d9ac3bd31da2f5bee8194
Hatatych <me@hataty.ch>
2019-01-26 23:01:23 +0300

    Lesson 2 Optimised algorythm

--- lesson-2/vowels.rb
+++ lesson-2/vowels.rb
@@ -1,6 +1,4 @@
-vowels = %w(а е ё и о у э ю я)
+vowels = "аеёиоуэюя"
 vowels_hash = Hash.new
-('а'..'я').each_with_index do |letter, index|
-  vowels_hash[letter] = index + 1 if vowels.include?(letter)
-end
+('а'..'я').each_with_index { |letter, index| vowels_hash[letter] = index + 1 if vowels[letter] }
 puts vowels_hash
